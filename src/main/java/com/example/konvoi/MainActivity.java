6dd0cb40f095baab75e3c3e8b2a4e23009466176
16ridd <45313886+16ridd@users.noreply.github.com>
2019-02-02 17:22:19 -0500

    Add files via upload

--- src/main/java/com/example/konvoi/MainActivity.java
+++ src/main/java/com/example/konvoi/MainActivity.java
@@ -1,17 +1,10 @@
 package com.example.konvoi;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
+import android.app.DownloadManager;
 import android.content.Intent;
-import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,18 +12,10 @@ import android.view.Menu;
 import android.app.Activity;
 
 
-
-import com.snapchat.kit.sdk.Bitmoji;
-import com.snapchat.kit.sdk.SnapLogin;
-import com.snapchat.kit.sdk.bitmoji.networking.FetchAvatarUrlCallback;
-import com.snapchat.kit.sdk.core.controller.LoginStateController;
-
-
-public class MainActivity extends AppCompatActivity implements LoginStateController.OnLoginStateChangedListener {
+public class MainActivity extends AppCompatActivity {
 
 
     private Button button;
-    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,66 +45,55 @@ public class MainActivity extends AppCompatActivity implements LoginStateControl
 
 
         });
-        SnapLogin.getButton(this, (ViewGroup) findViewById(R.id.login_button));
-        SnapLogin.getLoginStateController(this).addOnLoginStateChangedListener(this);
 
 
     }
 
-    @Override
-    public void onLoginSucceeded() {
-        findViewById(R.id.login_button).setVisibility(View.GONE);
-        Bitmoji.fetchAvatarUrl(this, new FetchAvatarUrlCallback() {
-            @Override
-            public void onSuccess(@Nullable String avatarUrl) {
-                Log.d("bitmoji", avatarUrl);
-            }
-
-            @Override
-            public void onFailure(boolean isNetworkError, int statusCode) {
-                Log.d("bitmoji","FUCK ME");
-            }
-        });
-    }
-
-    @Override
-    public void onLoginFailed() {
-        failedLogin();
-    }
-
-
-    public void failedLogin() {
-        AlertDialog.Builder loginError = new AlertDialog.Builder(context);
-        loginError.setTitle("THERE WAS A LOGIN ERROR \n PLEASE TRY AGAIN");
-
-        loginError.setMessage("this is the message")
-                .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        MainActivity.this.finish();
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-
-                    }
-                });
-
-        loginError.show();
-
-
-    }
-
-    @Override
-    public void onLogout() {
-
-    }
 
 //        button = (Button) findViewById(R.id.createTrip);
 //        button = (Button) findViewById(R.id.createTrip);
+
+//
+//    RequestQueue mRequestQueue;
+//
+//    // Instantiate the cache
+//    Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+//
+//    // Set up the network to use HttpURLConnection as the HTTP client.
+//    Network network = new BasicNetwork(new HurlStack());
+//
+//// Instantiate the RequestQueue with the cache and network.
+//    mRequestQueue = new RequestQueue(cache, network);
+//
+//// Start the queue
+//mRequestQueue.start();
+//
+//    String url ="http://10.217.11.57";
+//
+//    // Formulate the request and handle the response.
+//    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//            new Response.Listener<String>() {
+//                @Override
+//                public void onResponse(String response) {
+//                    // Do something with the response
+//                }
+//            },
+//            new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//                    // Handle error
+//                }
+//            });
+//
+//// Add the request to the RequestQueue.
+//mRequestQueue.add(stringRequest);
+//
+//
+
+
+
+
+
 
 }
 
