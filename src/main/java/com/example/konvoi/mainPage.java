109886c860a7698712296c4d8c409693569754b4
16ridd <45313886+16ridd@users.noreply.github.com>
2019-02-02 12:21:24 -0500

    Add files via upload

--- src/main/java/com/example/konvoi/mainPage.java
+++ src/main/java/com/example/konvoi/mainPage.java
@@ -1,13 +1,106 @@
 package com.example.konvoi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class mainPage extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class mainPage extends AppCompatActivity implements OnMapReadyCallback {
+    private Button button;
+    private GoogleMap mMap;
+    private MapView mapView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
+          setContentView(R.layout.activity_main_page);
+
+         mapView = findViewById(R.id.maps);
+         mapView.onCreate(savedInstanceState);
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        mapView.getMapAsync(this);
+
+
+
+//        button = (Button) findViewById(R.id.maps);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(mainPage.this, MapsActivity.class);
+//            }
+//        });
+
+
     }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        LatLng toronto = new LatLng(-34, 140);
+        LatLng london = new LatLng(-40, 130);
+
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(toronto).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(london).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mapView.onStart();
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        mapView.onResume();
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        mapView.onPause();
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    @Override
+    public void onSaveInstanceState(Bundle random) {
+        super.onSaveInstanceState(random);
+        mapView.onSaveInstanceState(random);
+    }
+    @Override
+    public void onLowMemory(){
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
+
+
 }
